a4512d8924784d62c2efa26ea5a2cd052042e596
FurkanCeyhan <ahmet.furkan.ceyhan@gmail.com>
2022-10-20 21:13:11 +0300

    new branch

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,18 +1,71 @@
-var express = require('express');
-var router = express.Router();
-
 const anaSayfa = function (req, res) {
-  res.render('anasayfa', { "title": "Anasayfa" });
-};
+  res.render('anasayfa', { "baslik": "Ana Sayfa","sayfaBaslik": {
+    "siteAd":"MekanBul",
+    "slogan":"Civardaki Mekanlari Kesfet!"
+  },
+  "mekanlar": [
+    {
+      "ad":"Piyola Cafe",
+      "adres":"Carsi",
+      "puan":"4",
+      "imkanlar": ["Kahveler","Kekler","Pastalar"],
+      "mesafe":"1km"
+    }
+    ,
+    {
+      "ad":"Zater Doner",
+      "adres":"Carsi Ici",
+      "puan":"3",
+      "imkanlar": ["Kahve","Cay","Pasta"],
+      "mesafe":"500m"
+    }
+  ]
+ });
+}
 
 const mekanBilgisi = function (req, res) {
-  res.render('mekanbilgisi', { "title": "Mekan bilgisi" });
-};
+  res.render('mekanbilgisi', {
+     "baslik": "Mekan bilgisi",
+      "mekanBaslik": "Yeni Mekan",
+      "mekanDetay": {
+        "ad":"Yeni Mekan",
+        "adres":"Yeni Adres",
+        "puan":"4",
+        "imkanlar": ["Yeni Kahveler"," Yeni Kekler","Pastalar"],
+        "koordinatlar": {
+           "enlem":"37.94",
+           "boylam":"30.59"
+        },
+        "saatler": [
+          {
+            "gunler":"Pazartesi-Cuma",
+            "acilis":"09.00",
+            "kapanis":"23.00",
+            "kapali": false
+          },
+          {
+            "gunler":"Cumartesi-Pazar",
+            "acilis":"10.00",
+            "kapanis":"22.00",
+            "kapali": false
+          }
+        ],
+        "yorumlar": [
+          {
+            "yorumYapan":"Ahmet Furkan Ceyhan",
+            "puan":"5",
+            "tarih":"20 Ekim 2022",
+            "yorumMetni":"Mukemmel"
 
-const yorumEkle = function (req, res) {
-  res.render('yorumekle', { "title": "Yorum ekle" });
-};
+          }
+        ]
+      }
+    });
+}
 
+const yorumEkle = function (req, res, next) {
+  res.render('yorumekle', { 'title': 'Yorum ekle' });
+};
 
 
 module.exports = {
@@ -20,4 +73,6 @@ module.exports = {
   anaSayfa,
   mekanBilgisi,
   yorumEkle
+
+
 }
